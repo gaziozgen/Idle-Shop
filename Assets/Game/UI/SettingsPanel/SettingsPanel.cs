76162b61d8e9978e922ff810ae091cd90801f836
osmantopal <Osmanntopall@hotmail.com>
2023-05-04 13:09:04 +0300

    dengeleme

--- Assets/Game/UI/SettingsPanel/SettingsPanel.cs
+++ Assets/Game/UI/SettingsPanel/SettingsPanel.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class SettingsPanel : FateMonoBehaviour
 {
     [SerializeField] private Canvas canvas = null;
+    [SerializeField] private Canvas option1Canvas = null;
+    [SerializeField] private Canvas option2Canvas = null;
 
     public void Open()
     {
         GameManager.Instance.PlayHaptic();
         GameManager.Instance.PauseGame();
         canvas.enabled = true;
+        option1Canvas.enabled = true;
+        option2Canvas.enabled = true;
     }
 
     public void Close()
@@ -19,5 +23,7 @@ public class SettingsPanel : FateMonoBehaviour
         GameManager.Instance.PlayHaptic();
         GameManager.Instance.ResumeGame();
         canvas.enabled = false;
+        option1Canvas.enabled = false;
+        option2Canvas.enabled = false;
     }
 }
